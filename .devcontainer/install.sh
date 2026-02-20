747f0abfa71590cbf4bd777e25c0a7d9e62b6056
Samantha Barron <sam@sambarron.me>
2026-02-20 12:38:36 +0000

    docker

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-SUDO=""
-if [ "$(id -u)" -ne 0 ]; then
-    SUDO="sudo"
-fi
-
+### HUGO
 HUGO_VERSION=$(curl -fsSL https://api.github.com/repos/gohugoio/hugo/releases/latest | grep '"tag_name"' | sed 's/.*"tag_name": "v\(.*\)".*/\1/')
 ARCH=$(uname -m)
 case "$ARCH" in
@@ -16,5 +12,24 @@ esac
 TMP_DIR=$(mktemp -d)
 curl -fsSL "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-${HUGO_ARCH}.tar.gz" \
     | tar -xz -C "$TMP_DIR" hugo
-$SUDO install -m 0755 "$TMP_DIR/hugo" /usr/local/bin/hugo
+sudo install -m 0755 "$TMP_DIR/hugo" /usr/local/bin/hugo
 rm -rf "$TMP_DIR"
+
+### DOCKER
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+# Install
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
