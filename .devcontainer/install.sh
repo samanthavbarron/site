14d5774ba3a7d28465bd484b549dc2ff4e0df8f0
Samantha Barron <sam@sambarron.me>
2026-02-21 16:51:40 +0000

    claude cli

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -33,3 +33,5 @@ EOF
 # Install
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin tmux
+
+curl -fsSL https://claude.ai/install.sh | bash
